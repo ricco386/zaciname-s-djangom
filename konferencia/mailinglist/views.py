2f10c676d8cf8b9646c64c0d98492efe9bfa1973
Richard Kellner <richard.kellner@erigones.com>
2015-03-11 23:40:24 +0100

    Added view that actually does something...

--- konferencia/mailinglist/views.py
+++ konferencia/mailinglist/views.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from mailinglist.models import MailingList
 
 def index(request):
     return HttpResponse(u'Ahoj Svet. Práve sa nachádzame v mailinglistovom indexe.')
 
 def mlist(request, list_id):
-    return HttpResponse(u'Práve sa nachádzame v mailinglite %s.' % list_id)
+    try:
+        ml = MailingList.objects.get(pk=list_id)
+    except MailingList.DoesNotExist:
+        raise Http404(u'Mailinglist neexistuje!!!')
+    return render(request, 'mailinglist/mlist.html', {'ml': ml})
 
 def subscribe(request, list_id):
     return HttpResponse(u'Práve sa zapisujete do mailinglistu %s.' % list_id)
