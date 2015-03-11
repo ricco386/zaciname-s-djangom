de524405be5f45144ed820001e210bd38d9b2d1a
Richard Kellner <richard.kellner@erigones.com>
2015-03-12 00:14:35 +0100

    Added form for subscription, updated subscribe view to store in DB, and template for rendering

--- konferencia/mailinglist/views.py
+++ konferencia/mailinglist/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, Http404
 from mailinglist.models import MailingList
+from mailinglist.forms import SubscribeForm
 
 def index(request):
     return HttpResponse(u'Ahoj Svet. Práve sa nachádzame v mailinglistovom indexe.')
@@ -14,4 +15,16 @@ def mlist(request, list_id):
     return render(request, 'mailinglist/mlist.html', {'ml': ml})
 
 def subscribe(request, list_id):
-    return HttpResponse(u'Práve sa zapisujete do mailinglistu %s.' % list_id)
+    ml = get_object_or_404(MailingList, pk=list_id)
+
+    if request.method == "POST":
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            subscriber = form.save()
+            ml.subscriber.add(subscriber)
+            return redirect('mlist', list_id=list_id)
+    else:
+        form = SubscribeForm
+
+    return render(request, 'mailinglist/subscribe.html', {'ml': ml, 'form': form})
+
