da39571755b2b5346c1945ab3316589053ec3483
Richard Kellner <richard.kellner@erigones.com>
2015-03-11 21:27:09 +0100

    Added initial view and urls definition

--- konferencia/konferencia/urls.py
+++ konferencia/konferencia/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'konferencia.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^ml/', include('mailinglist.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- konferencia/mailinglist/views.py
+++ konferencia/mailinglist/views.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse(u'Ahoj Svet. Práve sa nachádzame v mailinglistovom indexe.')
