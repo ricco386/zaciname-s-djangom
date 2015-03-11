4203b13d1ce2bed5a726bc066d53a5c5ff687949
Richard Kellner <richard.kellner@erigones.com>
2015-03-11 23:27:20 +0100

    Added extra views

--- konferencia/mailinglist/urls.py
+++ konferencia/mailinglist/urls.py
@@ -4,4 +4,6 @@ from mailinglist import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<list_id>\d+)/$', views.mlist, name='mlist'),
+    url(r'^(?P<list_id>\d+)/subscribe/$', views.subscribe, name='subscribe'),
 )
--- konferencia/mailinglist/views.py
+++ konferencia/mailinglist/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse(u'Ahoj Svet. Práve sa nachádzame v mailinglistovom indexe.')
+
+def mlist(request, list_id):
+    return HttpResponse(u'Práve sa nachádzame v mailinglite %s.' % list_id)
+
+def subscribe(request, list_id):
+    return HttpResponse(u'Práve sa zapisujete do mailinglistu %s.' % list_id)
